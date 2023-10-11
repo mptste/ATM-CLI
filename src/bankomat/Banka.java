2525498b8d9a9d53cf905c2b44209920655cfc04
mptste <124529697+mptste@users.noreply.github.com>
2023-10-11 12:07:16 +0200

    Update Banka.java

--- src/bankomat/Banka.java
+++ src/bankomat/Banka.java
@@ -6,37 +6,35 @@ package bankomat;
 import java.util.Scanner;
 
 /**
- *
  * @author pc
  */
 public class Banka {
-    
-      public static void main(String[] args) {
 
-          
-      Scanner sca = new Scanner(System.in);
-        
-      int pinKod = 2253, pin = 2253, count = 0;
-      
-      while (count < 3) {            
+    public static void main(String[] args) {
+
+
+        Scanner sca = new Scanner(System.in);
+
+        int pinKod = 2253, pin = 2253, count = 0;
+
+        while (count < 3) {
             count++;
             System.out.println("Pokusaj broj: " + count + " \nUnesite PIN kod: ");
             pinKod = sca.nextInt();
-            
+
             if (pinKod == pin) {
                 System.out.println("Validacija PIN kod-a uspesna");
                 Racun Milan = new Racun("Milan", "CT2588741");
                 Milan.prikazatiMeni();
                 break;
-              }
-            else{
+            } else {
                 System.out.println("Netacan PIN kod");
             }
         }
         if (count == 3) {
             System.out.println("Vas nalog je zakljucan nakon 3 neuspela pokusaja");
-            
+
         }
-      
+
     }
 
